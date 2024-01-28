b886e1b66faaaf918ede676a0c7d9ae7cb6d0e4a
david <david@autohandle.com>
2024-01-27 16:53:59 -0800

    added org.package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.pkg.HasDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world, it is 1/26/2024!");
+        System.out.println("1/26/2024 —>"+ HasDate.getDate());
     }
 }
---
+++ src/main/java/org/pkg/HasDate.java
@@ -0,0 +1,8 @@
+package org.pkg;
+
+public class HasDate {
+
+    public static String getDate() {
+        return "1/27/2024";
+    }
+}
